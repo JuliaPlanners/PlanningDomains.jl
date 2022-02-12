2f35c1d2a54789fc6db8c6dd243b44e678e4a752
Xuan <tanqazx@gmail.com>
2022-02-12 10:47:32 -0500

    Add tests for IPCInstancesRepo.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,30 @@
 using PlanningDomains, Test
+using PlanningDomains: clear_cache!, clear_all_caches!
 
 @testset "JuliaPlanners repository" begin
-    domains = list_domains(JuliaPlannersRepo)
-    @test !isempty(domains)
-    problems = list_problems(JuliaPlannersRepo, domains[1])
-    @test !isempty(problems)
-    load_domain(JuliaPlannersRepo, domains[1])
-    load_problem(JuliaPlannersRepo, domains[1], problems[1])
+    domain_names = list_domains(JuliaPlannersRepo)
+    @test !isempty(domain_names)
+    for dname in domain_names
+        problem_names = list_problems(JuliaPlannersRepo, dname)
+        @test !isempty(problem_names)
+        domain = load_domain(JuliaPlannersRepo, dname)
+        problem = load_problem(JuliaPlannersRepo, dname, problem_names[1])
+    end
 end
+
+@testset "IPC Instances repository" begin
+    domain_names = list_domains(IPCInstancesRepo)
+    @test !isempty(domain_names)
+    domain_names = list_domains(IPCInstancesRepo, "ipc-2000")
+    @test !isempty(domain_names)
+    for dname in domain_names
+        dname = "ipc-2000-$dname"
+        problem_names = list_problems(IPCInstancesRepo, dname)
+        @test !isempty(problem_names)
+        domain = load_domain(IPCInstancesRepo, dname)
+        problem = load_problem(IPCInstancesRepo, dname, problem_names[1])
+    end
+    clear_cache!(IPCInstancesRepo())
+end
+
+clear_all_caches!()
